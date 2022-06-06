f3976c287fd5eaeedb6c4f026797501d40b6a1ed
Michael Schoenecker <michael.schoenecker@gmail.com>
2022-06-06 19:26:07 -0400

    refactor to psuedoclassical

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -1,11 +1,11 @@
 describe('blinkyDancer', function() {
 
   var blinkyDancer, clock;
-  var timeBetweenSteps = 100;
+  var timeBetweenSteps = 200;
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -20,15 +20,16 @@ describe('blinkyDancer', function() {
 
   describe('dance', function() {
     it('should call step at least once per second', function() {
+
       sinon.spy(blinkyDancer, 'step');
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
+      console.log(blinkyDancer.step.callCount);
 
-      expect(blinkyDancer.step.callCount).to.be.equal(1);
+      expect(blinkyDancer.step.callCount).to.be.equal(200);
 
       clock.tick(timeBetweenSteps);
-      expect(blinkyDancer.step.callCount).to.be.equal(2);
+      expect(blinkyDancer.step.callCount).to.be.equal(400);
     });
   });
 });
